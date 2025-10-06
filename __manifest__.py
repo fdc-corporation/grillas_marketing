41685befbc5ea0b678c3c1a0bc50086ef37a7770
palaciosyost <palaciosyostin903@gmail.com>
2025-10-06 15:19:30 -0500

    Cambios migrados desde v17

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     """,
 
     'author': "Yostin Palacios",
-    'website': "https//fdc-corporation.com",
+    'website': "https//kauzadigital.pe",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
